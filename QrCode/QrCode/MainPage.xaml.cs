286aa4fe4d6a361b9e56ff86c5362a2eb4262a97
Marcelo Vargas <themartrix@hotmail.com>
2020-06-08 16:58:33 -0300

    Clean code in the MainPage.

--- QrCode/QrCode/MainPage.xaml.cs
+++ QrCode/QrCode/MainPage.xaml.cs
@@ -1,36 +1,35 @@
-﻿using QrCode.Services;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace QrCode
+﻿namespace QrCode
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
+    using QrCode.Services;
+    using System;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+
+
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        DialogService dialogService;
+        private readonly DialogService dialogService;
         public MainPage()
         {
             InitializeComponent();
             dialogService = new DialogService();
         }
 
-        private async void Button_OnClicked(object sender, EventArgs e) => await OpenScan();
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await OpenScan();
+        }
 
         private async Task OpenScan()
         {
-            var scanner = DependencyService.Get<IQrCodeScanningService>();
-            var result = await scanner.ScanAsync();
+            IQrCodeScanningService scanner = DependencyService.Get<IQrCodeScanningService>();
+            string result = await scanner.ScanAsync();
             if (!string.IsNullOrEmpty(result))
             {
-                
-                var QrCode = result;
+
+                string QrCode = result;
                 await dialogService.ShowMessage("QrCode", QrCode);
 
             }
